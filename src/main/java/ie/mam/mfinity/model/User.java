afa943984cb78025d257bf00cfa0dfc87d8cf7aa
marcioalmeid <marcioalmeid@gmail.com>
2018-05-26 19:35:29 -0400

    update user user repository user controller

---
+++ src/main/java/ie/mam/mfinity/model/User.java
@@ -0,0 +1,29 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String password;
+
+    protected User() {}
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, userName='%s', password='%s']",
+                id, userName, password);
+    }
+
+}
---
+++ src/main/java/ie/mam/mfinity/repository/UserRepository.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByUserName(String userName);
+}
