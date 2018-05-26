5f0a21351d2dc93b6477bddd5d9cd8c670db6a49
marcioalmeid <marcioalmeid@gmail.com>
2018-05-26 18:18:55 -0400

    Initialization of the MiFinityTest

---
+++ src/main/java/ie/mam/mfinity/MiFiniyTest/MiFiniyTestApplication.java
@@ -0,0 +1,12 @@
+package ie.mam.mfinity.MiFiniyTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiFiniyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiFiniyTestApplication.class, args);
+	}
+}
