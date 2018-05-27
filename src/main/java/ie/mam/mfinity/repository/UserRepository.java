c83cd708789069bc8ace3246b2a15e8c3b140bcd
marcioalmeid <marcioalmeid@gmail.com>
2018-05-26 21:18:01 -0400

    corrections

--- src/main/java/ie/mam/mfinity/repository/UserRepository.java
+++ src/main/java/ie/mam/mfinity/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package ie.mam.mfinity.repository;
 
 import java.util.List;
-
+import ie.mam.mfinity.model.User;
 import org.springframework.data.repository.CrudRepository;
+
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
