662f0397c9f2a096fa7499fca23e6b48f9ac2fc9
marcioalmeid <marcioalmeid@gmail.com>
2018-05-26 21:56:01 -0400

    corrections

--- src/main/java/ie/mam/mfinity/model/User.java
+++ src/main/java/ie/mam/mfinity/model/User.java
@@ -27,11 +27,11 @@ public class User {
                 id, userName, password);
     }
 
-    public Integer getId() {
+    public Long getId() {
       return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
       this.id = id;
     }
 
