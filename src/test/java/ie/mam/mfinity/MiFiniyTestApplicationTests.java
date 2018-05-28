b815889eeb5990fe7099bdde0d4872409fc06a19
marcioalmeid <marcioalmeid@gmail.com>
2018-05-28 10:31:59 -0400

    corrections

---
+++ src/test/java/ie/mam/mfinity/MiFiniyTestApplicationTests.java
@@ -0,0 +1,16 @@
+package ie.mam.mfinity.MiFiniyTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiFiniyTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
