860b2a1d90e1c0a0d572b73a98268cb95dbbd2cd
marcioalmeid <marcioalmeid@gmail.com>
2018-05-28 10:15:09 -0400

    corrections

--- src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
+++ src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
@@ -1,4 +1,4 @@
-package ie.mam.mfinity.MiFiniyTest;
+package ie.mam.mfinity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
