86b1ebe4d9f69463d9ae5ad84fc36958f2c05889
marcioalmeid <marcioalmeid@gmail.com>
2018-05-28 10:53:26 -0400

    corrections

--- src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
+++ src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
@@ -2,6 +2,12 @@ package ie.mam.mfinity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MiFiniyTestApplication {
@@ -11,4 +17,14 @@ public class MiFiniyTestApplication {
 	}
 
    
+}
+
+@RestController
+class IndexController{
+
+ @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
 }
