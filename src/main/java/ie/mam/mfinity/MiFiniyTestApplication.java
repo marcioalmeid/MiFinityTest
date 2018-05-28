a49b0cdf4e6d698d4258551f2bbbc6d2ce2f8307
marcioalmeid <marcioalmeid@gmail.com>
2018-05-28 10:13:07 -0400

    corrections

--- src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
+++ src/main/java/ie/mam/mfinity/MiFiniyTestApplication.java
@@ -10,20 +10,5 @@ public class MiFiniyTestApplication {
 		SpringApplication.run(MiFiniyTestApplication.class, args);
 	}
 
-
-@RestController
-@RequestMapping("/home")
-public class IndexController {
-    @RequestMapping("/")
-    String get() {
-        //mapped to hostname:port/home/
-        return "Hello from get";
-    }
-    @RequestMapping("/index")
-    String index() {
-        //mapped to hostname:port/home/index/
-        return "Hello from index";
-    }
-}
-  
+   
 }
