909596138522704d3c7581164a0739d24ad629b3
marcioalmeid <marcioalmeid@gmail.com>
2018-05-31 14:36:27 +0100

    CrossOrigin

--- src/main/java/ie/mam/mfinity/repository/UserRepository.java
+++ src/main/java/ie/mam/mfinity/repository/UserRepository.java
@@ -3,6 +3,8 @@ package ie.mam.mfinity.repository;
 import java.util.List;
 import ie.mam.mfinity.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://marcioalmeidamendes.ml:4200")
