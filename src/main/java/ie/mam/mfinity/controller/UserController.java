a7d091796164f617b8a8b907c7898e67173b820b
marcioalmeid <marcioalmeid@gmail.com>
2018-06-02 19:57:06 +0100

    PostMapping

--- src/main/java/ie/mam/mfinity/controller/UserController.java
+++ src/main/java/ie/mam/mfinity/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ie.mam.mfinity.model.User;
 import ie.mam.mfinity.repository.UserRepository;
@@ -37,6 +38,14 @@ public class UserController{
 //        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
+   @PostMapping(value="add")
+   @CrossOrigin(origins = "http://marcioalmeidamendes.ml:4200")
+    public User create(@RequestBody User user){
+        return userRepository.save(user);
+   }
+    
+    
+    
   @RequestMapping(value = "update/{id}", method = RequestMethod.POST)
   @CrossOrigin(origins = "http://marcioalmeidamendes.ml:4200")
   public @ResponseBody User update(@PathVariable("id") String id, @RequestBody User user) {
